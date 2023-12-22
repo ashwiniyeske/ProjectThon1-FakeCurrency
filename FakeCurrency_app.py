ecb45d8a9592c7d92608460ac9a79519434622d7
ashwiniyeske <144917650+ashwiniyeske@users.noreply.github.com>
2023-12-22 13:36:59 +0530

    Update FakeCurrency_app.py

--- FakeCurrency_app.py
+++ FakeCurrency_app.py
@@ -31,7 +31,7 @@ def main():
     # front end elements of the web page 
     html_temp = """ 
     <div style ="background-color:yellow;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">PragyanAI Fake Currency Detection Prediction ML App</h1> 
+    <h1 style ="color:black;text-align:center;">Ashwini_Yeske Fake Currency Detection Prediction ML App</h1> 
     </div> 
     """
       
